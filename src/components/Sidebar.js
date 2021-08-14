b21e4a34d75a4c64e19a8c57247e8fd51f168fb6
Junaed Abed <junaedccpc17@gmail.com>
2021-08-14 20:08:00 +0600

    sidebar updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
-  return <div className="sidebar">I am a sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://logos-download.com/wp-content/uploads/2016/08/Spotify_logo-700x210.png"
+        alt="logo"
+      />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
+    </div>
+  );
 }
 
 export default Sidebar;
