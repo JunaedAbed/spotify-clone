51a5130c8d3a45166d67f4efbf7d522ac8bc1829
Junaed Abed <junaedccpc17@gmail.com>
2021-08-16 05:28:36 +0600

    sidebar icon added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import AddIcon from "@material-ui/icons/Add";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 function Sidebar() {
   return (
@@ -10,9 +15,19 @@ function Sidebar() {
         src="https://logos-download.com/wp-content/uploads/2016/08/Spotify_logo-700x210.png"
         alt="logo"
       />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+      <br />
+      <SidebarOption title="Create Playlist" Icon={AddIcon} />
+      <SidebarOption title="Liked Songs" Icon={FavoriteBorderIcon} />
+
+      <hr />
+
+      <SidebarOption title="Rock" />
+      <SidebarOption title="Funk" />
+      <SidebarOption title="Pop" />
     </div>
   );
 }
